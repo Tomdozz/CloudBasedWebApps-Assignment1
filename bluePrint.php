bb15648e8bd7507b171a45cccc6f1c87655e247b
tomdozz <tom_1995@msn.com>
2018-04-18 11:49:59 +0200

    added blueprint and cleande up

---
+++ bluePrint.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Assigmen 1</title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <h1>Enhörningar</h1>
+    <hr>
+    <form class="" action="specific.php" method="get">
+      <div class="form-group">
+        <h4>Id på enhörning<h5>
+        <input type="text" name="search" class="form-control">
+        <input type="submit" name="searchBtn" class="btn btn-success" value="Visa Enhörning!">
+      </div>
+    </form>
+    <form class="" action="index.php">
+      <div class="form-group">
+        <input type="submit" name="searchBtn" class="btn btn-primary" value="Visa alla enhörningar!">
+      </div>
+    </form>
+  </body>
+</html>
--- specific.php
+++ specific.php
@@ -1,6 +1,7 @@
 <?php
 require("vendor/autoload.php");
 require("log.php");
+require("bluePrint.php");
 use GuzzleHttp\Client;
 $client = new Client();
 $res = $client->request('GET', 'http://unicorns.idioti.se/' . $_GET["search"],
@@ -8,31 +9,9 @@ $res = $client->request('GET', 'http://unicorns.idioti.se/' . $_GET["search"],
 $data = json_decode($res->getBody());
 $log->debug('Requested info about'. $data->name);
 ?>
-
 <!DOCTYPE html>
 <html>
-  <head>
-      <title>Assigmen 1</title>
-      <link rel="stylesheet" type="text/css" href="style.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </head>
   <body>
-    <h1>Enhörningar</h1>
-    <hr>
-    <form class="" action="specific.php" method="get">
-      <div class="form-group">
-        <h4>Id på enhörning<h5>
-        <input type="text" name="search" class="form-control">
-        <input type="submit" name="searchBtn" class="btn btn-success" value="Visa Enhörning!">
-      </div>
-    </form>
-    <form class="" action="index.php" method="post">
-      <div class="form-group">
-        <input type="submit" name="searchBtn" class="btn btn-primary" value="Visa alla enhörningar!">
-      </div>
-    </form>
     <div>
       <?php
         echo "<h2>". $data->name . "</h2>"
